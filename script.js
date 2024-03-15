e572e689f86468673672c5ddb7bf5735ea40e6a9
idevelopersclub <163556683+idevelopersclub@users.noreply.github.com>
2024-03-15 18:23:45 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+const words = ["برنامه نویس     ", "باهوش     ", "خبره     ","پایتون کار     ","بک اند کار     ","پی اچ پی کار     ","فرانت اند     ","علاقه مند     ","دور هم بودن     ","دوست پیدا کردن     "];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 400);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
+
+typingEffect();
